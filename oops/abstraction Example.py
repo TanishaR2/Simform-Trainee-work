248624a6218c6d8817da9d1edcaf4e2af8ce41ea
TanishaR2 <tanisha.ramani@simformsolutions.com>
2025-01-10 18:36:30 +0530

    Update and rename readme to abstraction Example.py

---
+++ oops/abstraction Example.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    
+    def make_sound(self):
+        return "Woof!"
+
+class Cat(Animal):
+    
+    def make_sound(self):
+        return "Meow!"
+
+def animal_sound(animal: Animal):
+    print(animal.make_sound())
+
+dog = Dog()
+cat = Cat()
+
+animal_sound(dog)
+animal_sound(cat)
