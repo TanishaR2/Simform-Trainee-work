1023bef9cade96034eb3bacbe11041ac11c8d1c2
TanishaR2 <tanisha.ramani@simformsolutions.com>
2025-01-15 18:38:04 +0530

    Add files via upload

---
+++ python basics/function/decorators/built-in decorators/class method decorator.py
@@ -0,0 +1,6 @@
+class Myclass:
+    @classmethod
+    def greet(cls, name):
+        print(f"Hello {name} from class method!")
+
+Myclass.greet("Richard")
---
+++ python basics/function/decorators/built-in decorators/static method decorator.py
@@ -0,0 +1,6 @@
+class Myclass:
+    @staticmethod
+    def say_hello():
+        print("Hello from static method!")
+
+Myclass.say_hello()
