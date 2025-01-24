182f2371372aaa612ee3361316c04ea1d95c41ea
TanishaR2 <tanisha.ramani@simformsolutions.com>
2025-01-24 15:53:41 +0530

    Update py problem3.py

--- Python Assignments/py problem3.py
+++ Python Assignments/py problem3.py
@@ -1,13 +1,10 @@
 strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-anagrams = {}  # Use a regular dictionary
+anagrams = {}
 
 for word in strs:
-    key = ''.join(sorted(word))  # Sort the word to create the key
-    if key not in anagrams:  # Check if the key is already in the dictionary
-        anagrams[key] = []  # Initialize the list if the key is missing
-    anagrams[key].append(word)  # Append the word to the list for the key
+    key = ''.join(sorted(word))
+    if key not in anagrams:
+        anagrams[key] = []
+    anagrams[key].append(word)
 
-output = list(anagrams.values())  # Convert the grouped values to a list of lists
-print(output)
-
-
+print(list(anagrams.values()))
