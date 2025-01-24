d580d535f83cf73ae72311f302056f3af98d4a84
TanishaR2 <tanisha.ramani@simformsolutions.com>
2025-01-24 15:52:14 +0530

    Update py problem2.py

--- Python Assignments/py problem2.py
+++ Python Assignments/py problem2.py
@@ -1,23 +1,3 @@
-# 2. Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-
-#     Constraints:
-
-#         - 1 <= n <= 8
-
-#     Example 1:
-
-#         - Input: n = 3
-
-#         - Output: ["((()))","(()())","(())()","()(())","()()()"]
-
-#         - Example 2:
-
-#     Example 2:
-
-#         - Input: n = 1
-
-#         - Output: ["()"] 
-
 from itertools import permutations
 def valid_para(com):
         balance = 0
